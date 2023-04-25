d61b0a9926ebac7435b13e1991a827b7b73d135e
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2023-04-25 19:10:59 +0200

    added more test. reaction and effect

--- src/effect.test.ts
+++ src/effect.test.ts
@@ -37,5 +37,125 @@ describe("effect", () => {
 
 			expect(result).toBe(undefined);
 		});
+
+		it("should set pending state on running", async () => {
+			const effect = new Effect(() => Promise.resolve("hello"));
+			const promise = effect.run();
+			expect(effect.isPending.get()).toBe(true);
+
+			await promise;
+		});
+
+		it("should abort if needed", async () => {
+			const abortListener = vi.fn();
+			const effect = new Effect(async (_data, abortController) => {
+				abortController.signal.addEventListener("abort", abortListener);
+				await Promise.resolve("hello");
+			});
+
+			effect.run();
+			effect.run();
+
+			expect(abortListener).toHaveBeenCalledTimes(1);
+		});
+
+		it("should set pending state on finished", async () => {
+			const effectFulfilled = new Effect(() => Promise.resolve("hello"));
+			const effectRejected = new Effect(() => Promise.resolve("error"));
+
+			expect(effectFulfilled.isPending.get()).toBe(false);
+			const effectFulfilledPromise = effectFulfilled.run();
+			expect(effectFulfilled.isPending.get()).toBe(true);
+			await effectFulfilledPromise;
+			expect(effectFulfilled.isPending.get()).toBe(false);
+
+			expect(effectRejected.isPending.get()).toBe(false);
+			const effectRejectedPromise = effectRejected.run();
+			expect(effectRejected.isPending.get()).toBe(true);
+			await effectRejectedPromise;
+			expect(effectRejected.isPending.get()).toBe(false);
+		});
+
+		it("should notify listeners on finished", async () => {
+			const listenerFulfilled = vi.fn();
+			const listenerRejected = vi.fn();
+			const effectFulfilled = new Effect(() => Promise.resolve("hello"));
+			const effectRejected = new Effect(() => Promise.reject("error"));
+
+			effectFulfilled.subscribe(listenerFulfilled);
+			effectRejected.subscribe(listenerRejected);
+
+			await Promise.all([effectFulfilled.run(), effectRejected.run()]);
+
+			expect(listenerFulfilled).toHaveBeenCalledTimes(1);
+			expect(listenerFulfilled).toHaveBeenCalledWith(
+				{
+					state: "fulfilled",
+					params: undefined,
+					result: "hello",
+				},
+				effectFulfilled
+			);
+			expect(listenerRejected).toHaveBeenCalledTimes(1);
+			expect(listenerRejected).toHaveBeenCalledWith(
+				{
+					state: "rejected",
+					params: undefined,
+					error: "error",
+				},
+				effectRejected
+			);
+		});
+	});
+
+	it("clone - should clone an effect with existing effect function", () => {
+		const effect = new Effect(() => Promise.resolve());
+
+		expect(effect.clone()).instanceOf(Entity);
+	});
+
+	it("abort - should abort effect and set change pending state", () => {
+		const abortListener = vi.fn();
+		const effect = new Effect(async (_data, abortController) => {
+			abortController.signal.addEventListener("abort", abortListener);
+			await Promise.resolve("hello");
+		});
+		effect.run();
+		expect(effect.isPending.get()).toBe(true);
+
+		effect.abort();
+		expect(effect.isPending.get()).toBe(false);
+		expect(abortListener).toHaveBeenCalledTimes(1);
+	});
+
+	it("release - should abort and remove subscribers", async () => {
+		const pendingSubscriber = vi.fn();
+		const effectSubscriber = vi.fn();
+		const effect = new Effect(() => Promise.resolve("hello"));
+		effect.abort = vi.fn();
+		effect.subscribe(effectSubscriber);
+		effect.isPending.subscribe(pendingSubscriber);
+
+		effect.release();
+
+		await effect.run();
+
+		expect(effect.abort).toHaveBeenCalledTimes(1);
+		expect(effectSubscriber).toHaveBeenCalledTimes(0);
+		expect(pendingSubscriber).toHaveBeenCalledTimes(0);
+	});
+
+	it("if happened auto-abort with specific error, subscribers should not be notified", async () => {
+		const subscriber = vi.fn();
+		const autoAbortEmulateEffect = new Effect(() => {
+			const abortError = new Error();
+			abortError.name = "AbortError";
+			return Promise.reject(abortError);
+		});
+		autoAbortEmulateEffect.subscribe(subscriber);
+
+		await autoAbortEmulateEffect.run();
+
+		expect(subscriber).toHaveBeenCalledTimes(0);
 	});
 });
---
+++ src/reaction.test.ts
@@ -0,0 +1,73 @@
+import { describe, it, expect, vi } from "vitest";
+import { Entity } from "./shared/entity";
+import { Reaction } from "./reaction";
+import { State } from "./state";
+
+describe("reaction", () => {
+	it("type", () => {
+		const state = new State("test");
+		const reaction = new Reaction(state, () => null);
+
+		expect(reaction).instanceOf(Entity);
+
+		reaction.release();
+	});
+
+	it("should call reaction-callback with state payload, when states changed", async () => {
+		const stat1 = new State("test");
+		const state2 = new State(0);
+		const reactionCallback = vi.fn();
+
+		const reaction = new Reaction(stat1, state2, reactionCallback);
+
+		stat1.set("new test");
+		// because callback invoked as microtask
+		await Promise.resolve();
+
+		expect(reactionCallback).toHaveBeenCalledTimes(1);
+		expect(reactionCallback).toHaveBeenCalledWith("new test", 0);
+
+		state2.set(1);
+		// because callback invoked as microtask
+		await Promise.resolve();
+
+		expect(reactionCallback).toHaveBeenCalledTimes(2);
+		expect(reactionCallback).toHaveBeenCalledWith("new test", 1);
+
+		reaction.release();
+	});
+
+	it("should call reaction-callback once even states change synchronously many times", async () => {
+		const stat1 = new State("test");
+		const state2 = new State(0);
+		const reactionCallback = vi.fn();
+
+		const reaction = new Reaction(stat1, state2, reactionCallback);
+
+		stat1.set("new test");
+		state2.set(1);
+		// because callback invoked as microtask
+		await Promise.resolve();
+
+		expect(reactionCallback).toHaveBeenCalledTimes(1);
+		expect(reactionCallback).toHaveBeenCalledWith("new test", 1);
+
+		reaction.release();
+	});
+
+	it("release - should unsubscribe from states", async () => {
+		const stat1 = new State("test");
+		const state2 = new State(0);
+		const reactionCallback = vi.fn();
+
+		const reaction = new Reaction(stat1, state2, reactionCallback);
+		reaction.release();
+
+		stat1.set("new test");
+		state2.set(1);
+		// because callback invoked as microtask
+		await Promise.resolve();
+
+		expect(reactionCallback).toHaveBeenCalledTimes(0);
+	});
+});
