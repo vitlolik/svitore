c1961ed2ca9f6b94136b7785e5fffb05bb8f3431
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2023-11-18 13:04:01 +0100

    updated packages and change build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-/// <reference types="vitest" />
-
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+	plugins: [
+		dts({
+			exclude: ["demo"],
+			rollupTypes: true,
+		}),
+	],
 	build: {
 		lib: {
 			entry: "./src/index.ts",
