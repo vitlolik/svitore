04e99ab00973b3aa7b98b619bfef580f21bc9c17
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2023-11-23 14:40:15 +0100

    added export

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -6,7 +6,7 @@ export { DebouncedEvent } from "./debounced-event";
 export { ThrottledEvent } from "./throttled-event";
 export { Effect } from "./effect";
 export { Reaction } from "./reaction";
-export { AbstractState, Entity } from "./services";
+export { AbstractState, AbstractEvent, Entity } from "./services";
 
 export type { EffectFunction } from "./effect";
 export type { Middleware } from "./services";
