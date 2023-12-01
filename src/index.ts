8fdcae02d0fdc08929e7a29172e243e292690159
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2023-12-01 22:18:00 +0100

    added export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Svitore } from "./svitore";
-export { AbstractEvent, AbstractState } from "./entities/services";
+export { AbstractEvent, AbstractState, Entity } from "./entities/services";
 
 export type { Middleware } from "./entities/services";
 export type { EffectFunction } from "./entities";
